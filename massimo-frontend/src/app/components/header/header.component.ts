7fd7411509e574a254ec5c9dfa1ad6f86b86e553
afdezcl <adrifdez_@uma.es>
2021-02-17 00:26:36 +0100

    :lipstick: MASS-7: Improve UI navbar-header

--- massimo-frontend/src/app/components/header/header.component.ts
+++ massimo-frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthenticationService,
+  ) { }
 
   ngOnInit() {
   }
 
+  onLogOut(): void {
+
+  }
 }
