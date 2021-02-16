9960fb51bc0435ed9661bdebaedec77940d02e92
afdezcl <adrifdez_@uma.es>
2021-02-16 15:20:54 +0100

    :sparkles: MASS-3: Set up login form

--- massimo-frontend/src/app/components/authentication/login/login.component.ts
+++ massimo-frontend/src/app/components/authentication/login/login.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 
-// JSON
-import usersList from 'src/assets/json/users.json';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -14,31 +11,26 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
   dataLoading = false;
-  users: any = usersList;
-  unregistered = false;
-  invalid = false;
 
   constructor(
-    private fb: FormBuilder,
+    private formBuilder: FormBuilder,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      username: [ '', [Validators.required, Validators.minLength(3)]],
-      password: [ '', [Validators.required, Validators.minLength(6)]]
+    this.initLoginForm();
+  }
+
+  initLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
   }
+
   loginUser() {
-    if (this.loginForm.invalid) { return; }
-    // TODO : Falta integrar el servicio para autentificar al usuario
-    // JSON simulando usuarios
-    const userLogin = this.loginForm.value.username;
-    const filterJson = this.users.filter((user) => user.first_name === userLogin);
-    if (filterJson.length > 0) {
-      this.router.navigate(['/principal/ships']);
-    } else {
-      this.unregistered = true;
+    if (this.loginForm.valid) {
+
     }
   }
 }
