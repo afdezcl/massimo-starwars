d0bdbe07f8637897cb01c67672604458e25fbbda
afdezcl <adrifdez_@uma.es>
2021-02-16 16:56:27 +0100

    :sparkles: MASS-3: Add login call in authentication service

--- massimo-frontend/src/app/services/authentication/authentication.service.ts
+++ massimo-frontend/src/app/services/authentication/authentication.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Register } from 'src/app/models/auth/register.interface';
+import { Login } from 'src/app/models/auth/login.interface';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +16,21 @@ export class AuthenticationService {
   register(user: Register): Observable<any> {
     return this.httpClient.post<any>(`${environment.apiAuthentication}/v1/users/register`, user);
   }
+
+  login(user: Login): Observable<any> {
+    return this.httpClient.post<any>(`${environment.apiAuthentication}/v1/users/login`, user)
+      .pipe(map(response => {
+        console.log(response);
+        this.setJwtToken(response);
+      }));
+  }
+
+  setJwtToken(token: string): void {
+    localStorage.setItem('JWT_TOKEN', token);
+  }
+
+  getJwtToken(): string {
+    return localStorage.getItem('JWT_TOKEN');
+  }
+
 }
