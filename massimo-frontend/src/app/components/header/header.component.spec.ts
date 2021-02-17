1c6d12fb857da3de456128f43d35cc05ef8d4240
afdezcl <adrifdez_@uma.es>
2021-02-17 01:17:23 +0100

    :heavy_plus_sign: MASS-7: Add toast dependencie to improve UX

--- massimo-frontend/src/app/components/header/header.component.spec.ts
+++ massimo-frontend/src/app/components/header/header.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -9,7 +10,8 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ HeaderComponent ],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule],
+      providers: [AuthenticationService]
     })
     .compileComponents();
   });
