f3d0a7c57e1f392669a5afaacdfa0346989b8325
afdezcl <adrifdez_@uma.es>
2021-02-18 12:39:56 +0100

    :heavy_plus_sign: MASS-15: Add login acceptance tests

---
+++ massimo-acceptance-tests/cypress/integration/authentication/login.spec.js
@@ -0,0 +1,35 @@
+import chance from 'chance';
+const globalVariables = require('../../fixtures/global-data.json')
+describe("Visit login page and sign in an user", () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it("Should sign in and navigate to ships page", () => {
+        cy.get('[data-cy=emailInput]')
+            .type(globalVariables.email)
+            .should('have.value', globalVariables.email);
+        cy.get('[data-cy=passwordInput]')
+            .type(globalVariables.password)
+            .should('have.value', globalVariables.password);
+        cy.get('[data-cy=signInButton]').click();
+        cy.get('.ng-trigger > .ng-tns-c19-0').contains('Welcome');
+        cy.location().should((location) => {
+            expect(location.href).to.eq('http://localhost:4200/ships')
+        });
+    });
+
+    it("Should NOT sign in and show alert", () => {
+        const password = chance().string({ length: 8 });
+        cy.get('[data-cy=emailInput]')
+            .type(globalVariables.email)
+            .should('have.value', globalVariables.email);
+        cy.get('[data-cy=passwordInput]')
+            .type(password)
+            .should('have.value', password);
+        cy.get('[data-cy=signInButton]').click();
+        cy.get('.ng-trigger > .ng-tns-c19-0').contains('Email or password was not correct');
+    });
+
+});
